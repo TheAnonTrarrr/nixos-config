1a20e5ecea6bf0e7c5447b1ded55db95be3c6e0c
TheAnonTrarrr <208259891+TheAnonTrarrr@users.noreply.github.com>
2026-04-26 23:08:49 +0300

    update

--- home/theanontrarrr/noctalia.nix
+++ home/theanontrarrr/noctalia.nix
@@ -1,9 +1,11 @@
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 
 {
   imports = [
     inputs.noctalia.homeModules.default
   ];
+
+  home.packages = [ inputs.noctalia.packages.${pkgs.system}.default ];
 
   programs.noctalia-shell = {
     enable = true;
