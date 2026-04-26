8517cb6d804a142cd90800adb6b2749570a7128f
TheAnonTrarrr <208259891+TheAnonTrarrr@users.noreply.github.com>
2026-04-26 22:33:19 +0300

    update

--- home/theanontrarrr/niri.nix
+++ home/theanontrarrr/niri.nix
@@ -12,23 +12,25 @@
     wayland-utils
   ];
 
-  programs.niri.settings = {
-    outputs."HDMI-A-1" = {
-      mode = { width = 1920; height = 1080; refresh = 74.97; };
-    };
+  programs.niri = {
+    settings = {
+      outputs."HDMI-A-1" = {
+        mode = { width = 1920; height = 1080; refresh = 74.97; };
+      };
 
-    binds = with config.lib.niri.actions; {
-      "Mod+Return".action = spawn "${pkgs.kitty}/bin/kitty";
-      "Mod+Q".action = close-window;
-      "Mod+Shift+E".action = quit;
+      binds = with config.lib.niri.actions; {
+        "Mod+Return".action = spawn "${pkgs.kitty}/bin/kitty";
+        "Mod+Q".action = close-window;
+        "Mod+Shift+E".action = quit;
       
-      "Mod+Left".action = focus-column-left;
-      "Mod+Right".action = focus-column-right;
-    };
+        "Mod+Left".action = focus-column-left;
+        "Mod+Right".action = focus-column-right;
+      };
 
-    layout = {
-      gaps = 10;
-      center-focused-column = true;
+      layout = {
+        gaps = 10;
+        center-focused-column = true;
+      };
     };
   };
 }
