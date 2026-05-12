c23aa790ad5a6235c4227f13cd4049e3f98b6f55
TheAnonTrarrr <208259891+TheAnonTrarrr@users.noreply.github.com>
2026-05-12 18:22:42 +0300

    update

--- home/theanontrarrr/niri.nix
+++ home/theanontrarrr/niri.nix
@@ -28,7 +28,7 @@
       };
 
       outputs."HDMI-A-5" = {
-        mode = { width = 1920; height = 1080; refresh = 84.0; };
+        mode = { width = 1920; height = 1080; refresh = 84.0; custom = true; };
       };
 
       binds = with config.lib.niri.actions; {
